98d780d46a6d58ad04fc13e3b457a87b929cc2b5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-04-23 21:51:32 +0200

    merged two wrappers to reduce edge cases

--- ssg/renderer.py
+++ ssg/renderer.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from collections import defaultdict
 from datetime import date, datetime
-from typing import NamedTuple
 from pathlib import Path
 
 
@@ -11,33 +10,6 @@ from .data import extract_global_data
 from .utils import copydir, write_text
 from .filters import redirect_path, resize_image
 from .pages import find_pages, render_content_to_html, get_page_collection, render_frontmatter, update_pages_data
-
-
-
-def _get_relative_output_path(collection_name: str | None, md_name: str) -> Path:
-    if md_name[0] == '_':
-        md_name = md_name[1:]
-    html_name = str(Path(md_name).with_suffix('.html'))
-    match collection_name, html_name:
-        case None, _: 
-            return Path(html_name)
-        case _: 
-            return Path(collection_name) / html_name
-        
-
-def _get_template_name(collection_name: str | None, md_name: str) -> str:
-    match collection_name, md_name:
-        case None, '_index.md': 
-            return f"index.html"
-        case None, _:
-            return str(Path(md_name).with_suffix('.html'))
-        case coll, '_index.md': 
-            return f"{coll}.html"
-        case coll, _:
-            assert coll[-1] == 's', "collection names should end with an s, dumb rule I know but here we are."
-            return f"{coll[:-1]}.html"
-
-
 
 
 def run_render_pipeline():
@@ -80,8 +52,8 @@ def run_render_pipeline():
         
         # Build HTML Page
         collection_name = get_page_collection(base_path='./pages', md_path=page_path)
-        template_name = _get_template_name(collection_name=collection_name, md_name=page_path.name)
-
+        template_name, rel_output_path = _get_template_name_and_output_path(collection_name=collection_name, md_name=page_path.name)
+        
         page_html = renderer.render_named_template(
             template_name=template_name,
             **{
@@ -90,12 +62,23 @@ def run_render_pipeline():
                 'page': page_data,
                 'pages': pages_data
             },
-        )
-        rel_output_path = _get_relative_output_path(collection_name=collection_name, md_name=page_path.name)
-        
+        )        
         write_text(base_dir='./output', file_path=rel_output_path, text=page_html)
 
     copydir(src="./static", target="./output/static")
 
 
 
+
+
+def _get_template_name_and_output_path(collection_name: str | None, md_name: str) -> tuple[str, Path]:
+    match collection_name, md_name:
+        case None, '_index.md': 
+            return ("index.html", Path('index.html'))
+        case None, md:
+            return (Path(md).with_suffix('.html').name, Path(md).with_suffix('.html').name)
+        case coll, '_index.md': 
+            return (f"{coll}.html", Path(coll) / 'index.html')
+        case coll, md:
+            assert coll[-1] == 's', "collection names should end with an s, dumb rule I know but here we are."
+            return (f"{coll[:-1]}.html", Path(coll) / Path(md).with_suffix('.html').name)
