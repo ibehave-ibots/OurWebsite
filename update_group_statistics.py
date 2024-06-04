43a5f28a2668c815c8a729816664e0a1374dd367
sangeethankumar <an.sangeetha@gmail.com>
2024-06-04 16:28:47 +0200

    estiamate short chat and hands on from txt file

--- update_group_statistics.py
+++ update_group_statistics.py
@@ -1,10 +1,19 @@
 import yaml
-from pprint import pprint
 
 with open('data/group.yaml') as group_f:
     group = yaml.safe_load(group_f)
 
-group["consulting_stats"]["num_consulting_sessions"] = 119
+
+with open('sangee_report.txt', 'r') as report:
+    content = report.read()
+
+num_short_chat = content.lower().count('type: short chat')
+num_hands_on = content.lower().count('type: hands-on')
+num_consulting_sessions = num_short_chat + num_hands_on
+total_hours_consulting = round(num_short_chat*0.75 + num_hands_on*2.5)
+
+group["consulting_stats"]["num_consulting_sessions"] = num_consulting_sessions
+group["consulting_stats"]["total_hours_consulting"] = total_hours_consulting
 
 with open('data/group.yaml', 'w') as update_group_f:
     yaml.safe_dump(group, update_group_f, default_flow_style=False)
