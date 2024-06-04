27faacb1eaf70b80cbbb3f7e7f2563be002e6dda
sangeethankumar <an.sangeetha@gmail.com>
2024-06-04 15:45:38 +0200

    update group statistics consulting session

--- update_group_statistics.py
+++ update_group_statistics.py
@@ -1,11 +1,11 @@
-# read group.yaml
-# update to new values
-# dump group.yaml
-
 import yaml
 from pprint import pprint
 
 with open('data/group.yaml') as group_f:
     group = yaml.safe_load(group_f)
 
-pprint(group["consulting_stats"])
+group["consulting_stats"]["num_consulting_sessions"] = 119
+
+with open('data/group.yaml', 'w') as update_group_f:
+    yaml.safe_dump(group, update_group_f, default_flow_style=False)
+
