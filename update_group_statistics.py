739cfca693342e239ecc1f3ab93a7ccd32c73c20
sangeethankumar <an.sangeetha@gmail.com>
2024-06-05 09:42:09 +0200

    automate count total sessions from all consultants

--- update_group_statistics.py
+++ update_group_statistics.py
@@ -27,19 +27,20 @@ fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(SANG
 reports = fs.ls("/", detail=False)
 fs.download("/", ".", recursive=True)
 
+num_consulting_sessions = 0
+for report in reports:
+    doc = Document(report)
+    num_consulting_sessions += count_page_breaks(doc)
 
-doc = Document('Sangee.docx.docx')
-page_break_count = count_page_breaks(doc)
-print(page_break_count)
 
 # num_short_chat = content.lower().count('type: short chat')
 # num_hands_on = content.lower().count('type: hands-on')
 # num_consulting_sessions = num_short_chat + num_hands_on
 # total_hours_consulting = round(num_short_chat*0.75 + num_hands_on*2.5)
 
-# group["consulting_stats"]["num_consulting_sessions"] = num_consulting_sessions
+group["consulting_stats"]["num_consulting_sessions"] = num_consulting_sessions
 # group["consulting_stats"]["total_hours_consulting"] = total_hours_consulting
 
-# with open('data/group.yaml', 'w') as update_group_f:
-#     yaml.safe_dump(group, update_group_f, default_flow_style=False)
+with open('data/group.yaml', 'w') as update_group_f:
+    yaml.safe_dump(group, update_group_f, default_flow_style=False)
 
