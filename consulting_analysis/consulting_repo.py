b626173068f69addf73d90cf02a331bce0184592
sangeethankumar <an.sangeetha@gmail.com>
2024-06-06 15:09:56 +0200

    test list method

--- consulting_analysis/consulting_repo.py
+++ consulting_analysis/consulting_repo.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel
-from typing import Any, Dict, Tuple
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+import json
+import os
+from pathlib import Path
+from typing import Any, Dict, NamedTuple, Tuple
 from pprint import pprint
+from pydantic import BaseModel
+import functools
+
+def requires_write_permission(fun):
+    @functools.wraps(fun)
+    def wrapper(*args, **kwargs):
+        if not os.getenv('DB_WRITEMODE'):
+            raise PermissionError("Write permission disabled")
+        return fun(*args, **kwargs)
+    return wrapper
 
 class ConsultingResult(BaseModel):
     short_name: str
@@ -9,22 +24,77 @@ class ConsultingResult(BaseModel):
     units: str
     display_units: str
 
-class ConsultingResultRepo(BaseModel):
-    consulting_results: list[ConsultingResult]
+@dataclass(frozen=True)
+class ResultDTO:
+    short_name: str
+    name: str
+    value: float
+    units: str
+    display_units: str
 
+@dataclass
+class ConsultingResultRepo:
+    _consulting_results: list[ConsultingResult] = field(repr=False)
+    path: Path
+
+    @classmethod
+    def connect(cls, path, write_mode = False) -> ConsultingResultRepo:
+        path = Path(path)
+        results = []
+        for file in path.glob('*.json'):
+            result = ConsultingResult.model_validate_json(file.read_text())
+            results.append(result)
+        
+        return ConsultingResultRepo(
+            _consulting_results=results,
+            path=path,
+        )
+
+    @requires_write_permission
     def clear_all(self) -> None: 
-        self.consulting_results.clear()
+        self._consulting_results.clear()
+            
+    @requires_write_permission
+    def put(self, short_name: str, name: str, value: float, units: str, display_units: str) -> None:
+        result = ConsultingResult(
+            short_name=short_name,
+            name=name,
+            value=value,
+            units=units,
+            display_units=display_units,
+        )
+        self._consulting_results.append(result)
 
-    def put(self, ConsultingResult) -> None:
-        self.consulting_results.append(ConsultingResult)
+    def list(self) -> list[ResultDTO]:
+        results = []
+        for result in self._consulting_results:
+            r = self.get(result.short_name)
+            results.append(r)
+        return results
 
-    def list_all(self) -> list[ConsultingResult]:
-        pprint(self.consulting_results)
-
-    def get(self, s_name: str) -> ConsultingResult:
-        for result in self.consulting_results:
+    def get(self, s_name: str) -> ResultDTO:
+        for result in self._consulting_results:
             if s_name in result.short_name:
-                return result
+                return ResultDTO(
+                    short_name=result.short_name,
+                    name=result.name,
+                    value=result.value,
+                    units=result.units,
+                    display_units=result.display_units
+
+                )
     
     def to_dict(self) -> Dict[Any, Any]:
-        return self.model_dump()
+        return self._asdict()
+    
+    def save(self):
+        path = Path(self.path)
+        if path.exists():
+            assert path.is_dir()
+        
+        path.mkdir(parents=True, exist_ok=True)
+        for i, result in enumerate(self._consulting_results):
+            json_text = result.model_dump_json(indent=3)
+            path.joinpath(f'{i}.json').write_text(json_text)
+        
+        
--- consulting_analysis/test_consulting_repo.py
+++ consulting_analysis/test_consulting_repo.py
@@ -1,53 +1,29 @@
-from . consulting_repo import ConsultingResult, ConsultingResultRepo
+import os
+from .consulting_repo import ConsultingResultRepo
 import pytest
 
-a = ConsultingResult(
-    short_name='n_sess',
-    name='Number of Sessions',
-    value=100,
-    units='Sessions',
-    display_units='Sess'
-)
+os.environ['DB_WRITEMODE'] = '1'
 
-b = ConsultingResult(
-    short_name='tot_hours',
-    name='Total Number of Hours',
-    value=10.5,
-    units='Hour',
-    display_units='Hrs'
-)
-
-c = ConsultingResult(
-    short_name='n_sc',
-    name='Number of Short Chats',
-    value=50,
-    units='Session',
-    display_units='Sess'
-)
-
-@pytest.fixture
-def results():
-    results = ConsultingResultRepo(
-        consulting_results=(a, b)
-    )
+@pytest.fixture()
+def results(tmp_path):
+    results = ConsultingResultRepo.connect(path=tmp_path)
+    results.put(short_name='a_b', name='A B', value=2, units='Hertz', display_units='Hz')
+    results.put(short_name='c_d', name='C D', value=20, units='Second', display_units='s')
+    results.put(short_name='e_f', name='E F', value=-0.5, units='Parts Per Million', display_units='ppm')
+    results.save()
     return results
 
 
-def test_repo_has_two_results(results):
-    assert len(results.consulting_results) == 2
+def test_list_method(results):
+    results_reports = results.list()
+    assert len(results.list()) == 3
+    assert results_reports[0].short_name == 'a_b'
+    assert results_reports[0].name == 'A B'
+    assert results_reports[0].value == 2
+    assert results_reports[0].units == 'Hertz'
+    assert results_reports[0].display_units == 'Hz'
 
-def test_put_method_adds_result(results):
-    results.put(c)
-    assert len(results.consulting_results) == 3
 
-def test_get_method_returns_n_sess_when_called(results):
-    n_sess = results.get('n_sess')
-    assert n_sess.short_name == 'n_sess'
-    assert n_sess.name == 'Number of Sessions'
-    assert n_sess.value == 100.0
-
-def test_len_clear_all_deletes_all_values_in_list(results):
-    assert len(results.consulting_results) == 2
-    results.clear_all()
-    assert len(results.consulting_results) == 0
-
+# def test_get_method_returns_n_sess_when_called(results):
+#     n_sess = results.get('n_sess')
+#     assert n_sess
