1dac3be4ddc82c6056df5bacf955beac3556eca6
sangeethankumar <an.sangeetha@gmail.com>
2024-06-11 13:19:24 +0200

    read directly from remote

--- consulting_analysis_results/src/sciebo_downloader.py
+++ consulting_analysis_results/src/sciebo_downloader.py
@@ -5,6 +5,7 @@ import pandas as pd
 from results_repo import ConsultingResultRepo
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 
 
 @dataclass
@@ -16,14 +17,12 @@ class ScieboConsultingStat:
     display_unit: str
 
 
-def get_from_sciebo() -> None:
+def from_sciebo() -> Dict[Any, Any]:
     load_dotenv()
     USR = os.getenv('USR')
     fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(USR, ""))
-    fs.download("/", "consulting_data", recursive=True)    
-
-def from_xlsx(path: str = 'consulting_data/consulting_stats.xlsx') -> Dict[Any, Any]:
-    df = pd.read_excel(path)
+    with fs.open('/consulting_stats.xlsx') as f:
+        df = pd.read_excel(f)
     consulting_stat_dict = {
         'short_name':df['short_name'].values,
         'name':df['name'].values,
@@ -33,12 +32,12 @@ def from_xlsx(path: str = 'consulting_data/consulting_stats.xlsx') -> Dict[Any, 
     } 
     return consulting_stat_dict
 
-def sciebo_to_repo():
+def sciebo_to_repo() -> ConsultingResultRepo:
     os.environ['DB_WRITEMODE'] = '1'
-    get_from_sciebo()
-    consulting_stat = from_xlsx()
+    consulting_stat_dict = from_sciebo()
     consulting_repo = ConsultingResultRepo.connect('consulting_data')
-    consulting_repo.from_dict(consulting_stat)
-    return consulting_repo.list()
+    consulting_repo.from_dict(consulting_stat_dict)
+    return consulting_repo
 
-print(sciebo_to_repo())
+cs = sciebo_to_repo()
+pprint(cs.get('num_researchers'))
