5633dcbed74fa85094bb6620b3af8dda441d08f8
sangeethankumar <an.sangeetha@gmail.com>
2024-06-18 10:47:51 +0200

    refactor write consulting stats

--- consulting_analysis/scripts/write_consulting_stats.py
+++ consulting_analysis/scripts/write_consulting_stats.py
@@ -5,6 +5,22 @@ import os
 import sys
 from pathlib import Path
 
+def process_consolidated_report(consolidated_report):
+    n_short = count_types_of_sessions(consolidated_report, type='short')
+    short_hrs = n_short * 0.45
+    n_hands_on = count_types_of_sessions(consolidated_report, type='hands')
+    hands_on_hrs = n_hands_on * 2.5
+    n_sess = n_short + n_hands_on
+    tot_hrs = short_hrs + hands_on_hrs
+    n_unique_scholars = count_num_unique_scholars(consolidated_report)
+    
+    return {
+        'n_sess': ('Total number of sessions', n_sess, 'Session', 'Session'),
+        'tot_hrs': ('Total hours of sessions', tot_hrs, 'Hour', 'Hrs'),
+        'n_hands': ('Total number of hands-on sessions', n_hands_on, 'Session', 'Session'),
+        'n_unique_scholars': ('Number of different scholars', n_unique_scholars, 'Researcher', 'Session')
+    }
+
 def main(consolidated_report_path):
     os.environ['DB_WRITEMODE'] = '1'
 
@@ -14,45 +30,16 @@ def main(consolidated_report_path):
     fs_local = LocalFileSystem()
     repo_local = ConsultingResultRepo.connect(fs_local)
 
-    n_short = count_types_of_sessions(consolidated_report, type='short')
-    short_hrs = n_short * 0.45
-    n_hands_on = count_types_of_sessions(consolidated_report, type='hands')
-    hands_on_hrs = n_hands_on * 2.5
-    n_sess = n_short + n_hands_on
-    tot_hrs = short_hrs + hands_on_hrs
-    n_unique_scholars = count_num_unique_scholars(consolidated_report)
+    results = process_consolidated_report(consolidated_report)
 
-    repo_local.put(
-        short_name='n_sess',
-        name='Total number of sessions',
-        value=n_sess,
-        units='Session',
-        display_units='Session'
-    )
-
-    repo_local.put(
-        short_name='tot_hrs',
-        name='Total hours of sessions',
-        value=tot_hrs,
-        units='Hour',
-        display_units='Hrs'
-    )
-
-    repo_local.put(
-        short_name='n_hands',
-        name='Total number of hands-on sesions',
-        value=n_hands_on,
-        units='Session',
-        display_units='Session'
-    )
-
-    repo_local.put(
-        short_name='n_unique_scholars',
-        name='Number of different scholars',
-        value=n_unique_scholars,
-        units='Researcher',
-        display_units='Researcher'
-    )
+    for short_name, (name, value, units, display_units) in results.items():
+        repo_local.put(
+            short_name=short_name,
+            name=name,
+            value=value,
+            units=units,
+            display_units=display_units
+        )    
 
     repo_local.push()
    
