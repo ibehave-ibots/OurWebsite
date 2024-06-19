dd6fe407fae639d1e7cd35f422889eb55e7883d9
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-06-19 15:06:05 +0200

    documented things better.

--- website/src/ssg/vendor_patches.py
+++ website/src/ssg/vendor_patches.py
@@ -2,12 +2,17 @@ import wrapt
 
 
 def patch_livereload_to_fix_bug_around_wsgi_support():  
+    """
+    This patch is to fix the error descrbed here: https://stackoverflow.com/questions/78039400/flask-with-livereload-and-tornado-causing-error-when-i-run-my-program
+
+    (Yes, I came up with this solution myself.  Use with caution!)
+    """
 
     from livereload.server import WSGIContainer
 
     @wrapt.patch_function_wrapper('livereload.server', 'LiveScriptContainer.__init__')
     def patched_init(wrapped, instance, args, kwargs):
-        print("Patched __init__ called")
+        """LiveScriptContainer should also call the __init__() from its superclass."""
         # Call the superclass __init__ first
         wsgi_app = args[0] if args else kwargs['wsgi_app']
         WSGIContainer.__init__(instance, wsgi_application=wsgi_app)
@@ -16,7 +21,11 @@ def patch_livereload_to_fix_bug_around_wsgi_support():
 
     @wrapt.patch_function_wrapper('livereload.server', 'LiveScriptContainer.__call__')
     def patched_call(wrapped, instance, args, kwargs):
-        print("Patched __call__ called...............................................")
+        """
+        LiveScriptContainer.__call__ is trying to call an instance method of its parent class as a static method.
+        The problem is that it was changed from a static method in some change, and this is what broke things.
+        Here, we trick the __call__ method into using itself instead of WSGIContainer when it references the WSGIContainer name.
+        """
         wrapped.__globals__['WSGIContainer'] = instance
         return wrapped(*args, **kwargs)
 
