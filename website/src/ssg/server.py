6f68b4d87fea5658877531b4f0a21f28b512cab9
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-06-19 15:08:34 +0200

    removed unnecessary path

--- website/src/ssg/server.py
+++ website/src/ssg/server.py
@@ -40,4 +40,4 @@ def build_server() -> Server:
     server.watch("static/**/*", func=run_render_pipeline)
     server.watch("pages/**/*", func=run_render_pipeline)
     server.watch("data/**/*", func=run_render_pipeline)
-    server.serve(root='./_output')
+    server.serve()
