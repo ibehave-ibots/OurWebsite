cbe92c6a0a9df0e519e222cacb134f78fc98f2e8
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-06-20 09:57:09 +0200

    added explicit response

--- website/src/ssg/server.py
+++ website/src/ssg/server.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 from livereload import Server
-from flask import Flask, abort, redirect, send_from_directory
+from flask import Flask, abort, redirect, send_from_directory, make_response
 
 from .vendor_patches import patch_livereload_to_fix_bug_around_wsgi_support
 from .renderer import run_render_pipeline
@@ -22,7 +23,10 @@ def build_server() -> Server:
             return redirect('index.html')  # appends 'index.html' to the path for directory-style requests (e.g. 'events/' -> 'events/index.html')
         
         if os.path.isfile(os.path.join(STATIC_FOLDER, path)):
-            return send_from_directory(STATIC_FOLDER, path)
+            response =  make_response(send_from_directory(STATIC_FOLDER, path ))
+            if Path(path).suffix == '.html':
+                response.headers['Content-Type'] = 'text/html; charset=UTF-8'
+            return response
         else:
             # Return a 404 error if the file does not exist
             print('could not find:', path)
