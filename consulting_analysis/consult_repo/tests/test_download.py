5096557dd2d175bcb2dc2f41ab38655817c0bd69
sangeethankumar <an.sangeetha@gmail.com>
2024-06-26 10:37:47 +0200

    correct test assert (download)

--- consulting_analysis/consult_repo/tests/test_download.py
+++ consulting_analysis/consult_repo/tests/test_download.py
@@ -13,4 +13,4 @@ def test_download_raw_reports(download_raw):
     sciebo_download = ScieboDataDownload()
     sciebo_download.download_raw_reports(destination=str(download_raw))
     fs_raw = LocalFileSystem()
-    assert len(fs_raw.ls(str(download_raw), detail=False)) == 3
+    assert len(fs_raw.ls(str(download_raw), detail=False)) > 0
