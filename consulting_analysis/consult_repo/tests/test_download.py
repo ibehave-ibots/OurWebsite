5f0d758a56d1641a81eb93753279089273f8223e
sangeethankumar <an.sangeetha@gmail.com>
2024-06-26 10:17:38 +0200

    split to src (download)

--- consulting_analysis/consult_repo/tests/test_download.py
+++ consulting_analysis/consult_repo/tests/test_download.py
@@ -1,34 +1,6 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-from dotenv import load_dotenv
-import os
 from fsspec.implementations.local import LocalFileSystem
-from webdav4.fsspec import WebdavFileSystem
 import pytest
-
-
-class DataDownloadStrategy(ABC):
-    @abstractmethod
-    def download_raw_reports(self, destination: str) -> None:
-        pass
-
-    @abstractmethod
-    def download_results(self, destination: str) -> None:
-        pass
-
-class ScieboDataDownload(DataDownloadStrategy):
-    def download_raw_reports(self, destination: str):
-        load_dotenv()
-        SANGEE_REPORT_USR = os.getenv('SANGEE_REPORT_USR')
-        SANGEE_REPORT_PWD = os.getenv('SANGEE_REPORT_PWD')
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(SANGEE_REPORT_USR, SANGEE_REPORT_PWD))
-        fs.download("/", destination, recursive=True)
-
-    def download_results(self, destination: str):
-        load_dotenv()
-        SANGEE_RESULTS_USR = os.getenv('USR')
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(SANGEE_RESULTS_USR, ''))
-        fs.download("/", destination, recursive=True)
+from src import ScieboDataDownload
 
 @pytest.fixture
 def download_raw(tmp_path):
