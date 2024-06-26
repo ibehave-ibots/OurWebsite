c0a75a49273c956d2610af8f06f282be3c4a8e5f
sangeethankumar <an.sangeetha@gmail.com>
2024-06-26 10:36:55 +0200

    correct test

--- consulting_analysis/consult_repo/tests/test_process.py
+++ consulting_analysis/consult_repo/tests/test_process.py
@@ -19,4 +19,4 @@ def test_process_string_is_not_empty(download_raw):
     fs_raw = LocalFileSystem()
     reports = fs_raw.ls(str(download_raw))
 
-    assert len(word_doc.process(reports)) != 0
+    assert len(word_doc.process(reports)) > 0
