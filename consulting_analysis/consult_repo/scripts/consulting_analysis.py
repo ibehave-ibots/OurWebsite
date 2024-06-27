f8bd6323f2e3da31b0759a942ad9747ae58b805f
sangeethankumar <an.sangeetha@gmail.com>
2024-06-27 10:43:39 +0200

    add sample results after restructure

--- consulting_analysis/consult_repo/scripts/consulting_analysis.py
+++ consulting_analysis/consult_repo/scripts/consulting_analysis.py
@@ -1,9 +1,12 @@
+import os
 from fsspec.implementations.local import LocalFileSystem
 from src import ScieboDataDownload  
 from src import WordDocumentProcessor
 from src import count_types_of_sessions, count_num_unique_scholars, count_num_occurrances_of_word
+from results_repo import ConsultingResultRepo
 
 def main():
+    os.environ['DB_WRITEMODE'] = '1'
     fs_raw = LocalFileSystem()
 
     if not fs_raw.exists('raw/', detail=False):
@@ -22,10 +25,27 @@ def main():
     n_python = count_num_occurrances_of_word(processed_text, word='python')
     n_matlab = count_num_occurrances_of_word(processed_text, word='matlab')
 
-    print(n_matlab)
+    consulting_results = [
+        n_short,
+        n_hands,
+        n_unique_scholars,
+        n_python,
+        n_matlab
+    ]
 
+    fs_local = LocalFileSystem()
+    repo_local = ConsultingResultRepo.connect(fs_local)
+    for consulting_result in consulting_results:
+        repo_local.put(
+            short_name=consulting_result.short_name,
+            name=consulting_result.name,
+            value=consulting_result.value,
+            units=consulting_result.units,
+            display_units=consulting_result.display_units
+        )
 
-    
+    repo_local.push()
+
 
 if __name__ == "__main__":
     main()
