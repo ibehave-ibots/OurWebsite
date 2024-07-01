54edc99bc1a51d2e52b61cf1970962ebc46c24c5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-01 13:44:14 +0200

    refactored a bit

--- website/src/ssg/data.py
+++ website/src/ssg/data.py
@@ -1,25 +1,30 @@
-from typing import Any
+from typing import Any, Dict
 from pathlib import Path, PurePosixPath
 from collections import defaultdict
 from .utils import load_yaml
 
 
-def extract_global_data(base_path: Path) -> dict[str, Any]:
-    def extract_recursive(path: Path) -> Any:
-        path = Path(path)
-        collections = {}
-        for item in path.iterdir():
-            if item.is_dir():
-                collections[item.stem] = extract_recursive(item)
-            else:
-                if '.yaml' in item.name:
-                    data = load_yaml(item.read_text())
-                    if item.name == '.yaml':
-                        collections.update(data)  # Merge directly if filanme only '.yaml'
-                    else:
-                        collections[item.stem] = data
-                else:
-                    collections[item.stem] = str(PurePosixPath(item.relative_to(base_path)))
-        return collections
+def extract_global_data(base_path: Path) -> Dict[str, Any]:
+    return _extract_recursive(base_path, base_path)
 
-    return extract_recursive(base_path)
+
+def _process_file(item: Path, base_path: Path, collections: Dict[str, Any]) -> None:
+    if item.name == '.yaml':  # Is only a YAML extension?  Read it and just stick the data onto the parent.
+        data = load_yaml(item.read_text())
+        collections.update(data)  
+    elif '.yaml' in item.name:  # Has a YAML extension?  Read it and use the filename's id as the key.
+        data = load_yaml(item.read_text())
+        collections[item.stem] = data
+    else:  # Not sure how to parse the file?  Then assign a relative filepath.
+        collections[item.stem] = str(PurePosixPath(item.relative_to(base_path)))
+
+
+def _extract_recursive(path: Path, base_path: Path) -> Dict[str, Any]:
+    collections = {}
+    for item in path.iterdir():
+        if item.is_dir():
+            collections[item.stem] = _extract_recursive(item, base_path)
+        else:
+            _process_file(item, base_path, collections)
+    return collections
+
