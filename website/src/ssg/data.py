4009916d99da2dffb7232ad54c921a55402ac31b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-01 12:44:20 +0200

    R: seperate out extension detection from file finding

--- website/src/ssg/data.py
+++ website/src/ssg/data.py
@@ -7,21 +7,24 @@ from .utils import load_yaml
 def extract_global_data(base_path: Path) -> dict[str, Any]:
     base_path = Path(base_path)
     collections = {}
-    for path in base_path.glob('*.yaml'):
-        data = load_yaml(path.read_text())
-        collections[path.stem] = data
-    for path in base_path.glob('*/*.yaml'):
-        data = load_yaml(path.read_text())
+    for path in base_path.glob('*.*'):
+        if path.suffix == '.yaml':
+            data = load_yaml(path.read_text())
+            collections[path.stem] = data
+    for path in base_path.glob('*/*.*'):
         if not path.parent.stem in collections:
             collections[path.parent.stem] = {}
-        collections[path.parent.stem][path.stem] = data
-    for path in base_path.glob('*/*/*.yaml'):
-        data = load_yaml(path.read_text())
+        if path.suffix == '.yaml':
+            data = load_yaml(path.read_text())
+            collections[path.parent.stem][path.stem] = data
+    for path in base_path.glob('*/*/*.*'):
         if not path.parent.parent.stem in collections:
             collections[path.parent.parent.stem] = {}
         if not path.parent.stem in collections[path.parent.parent.stem]:
             collections[path.parent.parent.stem][path.parent.stem] = {}
-        collections[path.parent.parent.stem][path.parent.stem][path.stem] = data
+        if path.suffix == '.yaml':
+            data = load_yaml(path.read_text())
+            collections[path.parent.parent.stem][path.parent.stem][path.stem] = data
     for path in base_path.glob('*/*/*'):
         if path.is_dir():
             raise NotImplementedError("only single- and double-nested collections are currently supported, sorry.")
