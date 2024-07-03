eb24d3a397201caffbce0f0c6b2a31ca7abf4ac8
sangeethankumar <an.sangeetha@gmail.com>
2024-07-03 14:14:20 +0200

    external sciebo

--- consulting_analysis/consult_repo/src/adapters/downloadrepo_sciebo.py
+++ consulting_analysis/consult_repo/src/adapters/downloadrepo_sciebo.py
@@ -1,29 +1,7 @@
-from dataclasses import dataclass
-import os
-from typing import Union
-from dotenv import load_dotenv
+from .scieboexternal import load_report_credentials, load_result_credentials
 from src.app import DataDownload
 from webdav4.fsspec import WebdavFileSystem
 
-
-@dataclass
-class Credentials:
-    USR: str
-    PWD: str
-
-def load_report_credentials() -> Credentials:
-    load_dotenv()
-    return Credentials(
-        USR = os.getenv('REPORT_USR') or '',
-        PWD = os.getenv('REPORT_PWD') or ''
-    )
-
-def load_result_credentials() -> Credentials:
-    load_dotenv()
-    return Credentials(
-        USR = os.getenv('RESULT_USR') or '',
-        PWD = os.getenv('RESULT_PWD') or ''
-    )
 
 class ScieboDataDownload(DataDownload):
     def download_reports(self, destination: str):
