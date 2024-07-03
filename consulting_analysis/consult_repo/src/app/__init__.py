711a4cb3948583435c1a57039c7ce247e5a696e4
sangeethankumar <an.sangeetha@gmail.com>
2024-07-03 15:20:30 +0200

    process refactor same folder

--- consulting_analysis/consult_repo/src/app/__init__.py
+++ consulting_analysis/consult_repo/src/app/__init__.py
@@ -1,3 +1,4 @@
 from .download import DataDownload
 from .upload import DataUpload
-from .process import TemplateDocumentProcessor
+from .process import DocumentProcessor
+from .processrepo_template import TemplateDocumentProcessor
