044640193429645c3a5831700c5c8f6aa1ebac2b
sangeethankumar <an.sangeetha@gmail.com>
2024-07-03 15:13:06 +0200

    upload refactor

--- consulting_analysis/consult_repo/src/adapters/uploadrepo_sciebo.py
+++ consulting_analysis/consult_repo/src/adapters/uploadrepo_sciebo.py
@@ -1,12 +1,10 @@
 from dotenv import load_dotenv
 from src import DataUpload
+from src.external import load_result_credentials
 from webdav4.fsspec import WebdavFileSystem
-import os
 
 class ScieboDataUpload(DataUpload):
     def connect(self) -> WebdavFileSystem:
-        load_dotenv()
-        RESULT_USR = os.getenv('RESULT_USR') or '' 
-        RESULT_PWD = os.getenv('RESULT_PWD') or ''
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(RESULT_USR, RESULT_PWD))
+        credentials = load_result_credentials()
+        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(credentials.USR, credentials.PWD))
         return fs
