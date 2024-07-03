c3411ef3a836f7f0b2e5742cb227b6930033329d
sangeethankumar <an.sangeetha@gmail.com>
2024-07-03 09:58:50 +0200

    refactor data download

--- consulting_analysis/consult_repo/src/download.py
+++ consulting_analysis/consult_repo/src/download.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import Union
 from dotenv import load_dotenv
 from webdav4.fsspec import WebdavFileSystem
 import os
@@ -15,15 +16,20 @@ class DataDownload(ABC):
 
 class ScieboDataDownload(DataDownload):
     def download_raw_reports(self, destination: str):
-        load_dotenv()
-        REPORT_USR = os.getenv('REPORT_USR')
-        REPORT_PWD = os.getenv('REPORT_PWD')
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(REPORT_USR, REPORT_PWD))
+        fs = self._get_file_system('report')
         fs.download("/", destination, recursive=True)
 
     def download_results(self, destination: str):
+        fs = self._get_file_system('result')
+        fs.download("/", destination, recursive=True)
+
+    def _get_file_system(self, type: Union['report': str, 'result': str]) -> WebdavFileSystem:
         load_dotenv()
+        REPORT_USR = os.getenv('REPORT_USR')
+        REPORT_PWD = os.getenv('REPORT_PWD')
         RESULT_USR = os.getenv('REPORT_USR')
         RESULT_PWD = os.getenv('REPORT_PWD')
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(RESULT_USR, RESULT_PWD))
-        fs.download("/", destination, recursive=True)
+        if type == 'report':
+            return WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(REPORT_USR, REPORT_PWD))
+        if type == 'result':
+            return WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(RESULT_USR, RESULT_PWD))
