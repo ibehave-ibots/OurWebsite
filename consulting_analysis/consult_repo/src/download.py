366dec7ff1baf0e336f32bc625e3ec48d477abac
sangeethankumar <an.sangeetha@gmail.com>
2024-07-03 09:49:06 +0200

    corrected env variables

--- consulting_analysis/consult_repo/src/download.py
+++ consulting_analysis/consult_repo/src/download.py
@@ -16,14 +16,14 @@ class DataDownload(ABC):
 class ScieboDataDownload(DataDownload):
     def download_raw_reports(self, destination: str):
         load_dotenv()
-        DOWNLOAD_USR = os.getenv('DOWNLOAD_USR')
-        DOWNLOAD_PWD = os.getenv('DOWNLOAD_PWD')
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(DOWNLOAD_USR, DOWNLOAD_PWD))
+        REPORT_USR = os.getenv('REPORT_USR')
+        REPORT_PWD = os.getenv('REPORT_PWD')
+        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(REPORT_USR, REPORT_PWD))
         fs.download("/", destination, recursive=True)
 
     def download_results(self, destination: str):
         load_dotenv()
-        UPLOAD_USR = os.getenv('UPLOAD_USR')
-        UPLOAD_PWD = os.getenv('UPLOAD_PWD')
-        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(UPLOAD_USR, UPLOAD_PWD))
+        RESULT_USR = os.getenv('REPORT_USR')
+        RESULT_PWD = os.getenv('REPORT_PWD')
+        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(RESULT_USR, RESULT_PWD))
         fs.download("/", destination, recursive=True)
