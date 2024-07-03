1cab71e9bcf26e2dbb92ae9abf14100d37dd10de
sangeethankumar <an.sangeetha@gmail.com>
2024-07-03 14:11:53 +0200

    split responsibilities sciebo download

--- consulting_analysis/consult_repo/src/adapters/downloadrepo_sciebo.py
+++ consulting_analysis/consult_repo/src/adapters/downloadrepo_sciebo.py
@@ -1,25 +1,37 @@
+from dataclasses import dataclass
 import os
 from typing import Union
 from dotenv import load_dotenv
 from src.app import DataDownload
 from webdav4.fsspec import WebdavFileSystem
 
+
+@dataclass
+class Credentials:
+    USR: str
+    PWD: str
+
+def load_report_credentials() -> Credentials:
+    load_dotenv()
+    return Credentials(
+        USR = os.getenv('REPORT_USR') or '',
+        PWD = os.getenv('REPORT_PWD') or ''
+    )
+
+def load_result_credentials() -> Credentials:
+    load_dotenv()
+    return Credentials(
+        USR = os.getenv('RESULT_USR') or '',
+        PWD = os.getenv('RESULT_PWD') or ''
+    )
+
 class ScieboDataDownload(DataDownload):
     def download_reports(self, destination: str):
-        fs = self._get_file_system('report')
+        credentials = load_report_credentials()
+        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(credentials.USR, credentials.PWD))
         fs.download("/", destination, recursive=True)
 
     def download_results(self, destination: str):
-        fs = self._get_file_system('result')
+        credentials = load_result_credentials()
+        fs = WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(credentials.USR, credentials.PWD))
         fs.download("/", destination, recursive=True)
-
-    def _get_file_system(self, type: Union['report': str, 'result': str]) -> WebdavFileSystem:
-        load_dotenv()
-        REPORT_USR = os.getenv('REPORT_USR') or ''
-        REPORT_PWD = os.getenv('REPORT_PWD') or ''
-        RESULT_USR = os.getenv('RESULT_USR') or ''
-        RESULT_PWD = os.getenv('RESULT_PWD') or ''
-        if type == 'report':
-            return WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(REPORT_USR, REPORT_PWD))
-        if type == 'result':
-            return WebdavFileSystem("https://uni-bonn.sciebo.de/public.php/webdav", auth=(RESULT_USR, RESULT_PWD))
