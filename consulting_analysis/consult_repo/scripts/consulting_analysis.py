00f7d390c8e402ea1603ecfe07bba638c4549728
sangeethankumar <an.sangeetha@gmail.com>
2024-07-05 10:33:16 +0200

    changed logic num scholars

--- consulting_analysis/consult_repo/scripts/consulting_analysis.py
+++ consulting_analysis/consult_repo/scripts/consulting_analysis.py
@@ -12,6 +12,8 @@ def main():
         sciebo_download = ScieboDataDownload()
         sciebo_download.download_reports(destination='raw/')
 
+
+
     template_doc = TemplateDocumentProcessor()
     reports = fs_raw.ls('raw/', detail=False)
     extracted_consultants = template_doc.process(reports_path=reports)
@@ -19,7 +21,7 @@ def main():
     total_sessions = sum(consultant.num_total_sessions for consultant in consultants)
     num_short_sessions = sum(consultant.num_short_sessions for consultant in consultants)
     num_hands_on_sessions = sum(consultant.num_hands_on_sessions for consultant in consultants)
-    num_unique_scholars = sum(consultant.num_unique_scholars for consultant in consultants)
+    num_unique_scholars = len(set().union(*[consultant.scholars for consultant in consultants]))
     time_short_hrs = sum(consultant.time_short_hrs for consultant in consultants)
     time_hands_on_hrs = sum(consultant.time_hands_on_hrs for consultant in consultants)
     time_all_hrs = sum(consultant.time_all_hrs for consultant in consultants)
