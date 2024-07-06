95a7dd678e18b1bccb328b6e9f39567bc9143612
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-06 09:03:39 +0200

    break up filters into submodules

---
+++ website/src/ssg/filters/files.py
@@ -0,0 +1,55 @@
+import urllib.request
+from pathlib import Path, PurePosixPath
+
+from PIL import Image
+
+
+def redirect_path(prepend_path, arg_idx=0):
+    """
+    Decorator that redirects the path onto the first argument of the function for internel use, but keeps
+    the output path the same. 
+    """
+    def decorator(fun):
+        def wrapper(*args, **kwargs):
+            path = args[arg_idx]
+            if str(path).startswith('/'):
+                was_root = True
+                path = path[1:]
+            else:
+                was_root = False
+            if 'http' in str(path):
+                base_url = path[path.index('//') + 2:]
+                new_path = str(PurePosixPath(prepend_path) / 'downloads' / base_url)
+                Path(new_path).parent.mkdir(exist_ok=True, parents=True)
+                print(f"Downloading File: {path} -> {new_path}")
+                urllib.request.urlretrieve(path, new_path)
+            else:
+                new_path = str(PurePosixPath(prepend_path).joinpath(path))
+            new_args = list(args)
+            new_args[arg_idx] = new_path
+            new_args = tuple(new_args)
+            output_path = fun(*new_args, **kwargs)
+            new_path = str(PurePosixPath(Path(output_path).relative_to(prepend_path)))
+            if was_root:
+                new_path = '/' + new_path
+            return new_path
+        return wrapper
+    return decorator
+
+
+def resize_image(fname: str, width: int, height: int) -> str:
+    path = Path(fname)
+    with Image.open(path) as img:
+        img2 = img.resize((width, height))
+
+    path2 = path.with_stem(path.stem + f"_{width}x{height}")
+    img2.save(path2)
+    return str(PurePosixPath(path2))
+
+
+def prepend(path: str, base: str) -> str:
+    if PurePosixPath(path).is_absolute() or Path(path).is_absolute():  # here we also use linux-style filenames in a windows environment.
+        raise ValueError(f"absolute path {path} cannot be prepended.")
+
+    new_path = PurePosixPath(base).joinpath(path)
+    return str(new_path)
--- website/src/ssg/tests/filters/test_data_filters.py
+++ website/src/ssg/tests/filters/test_data_filters.py
@@ -1,27 +1,8 @@
-import pytest
+
 from ssg import filters
 
+from ssg.filters import data_structures
 
-def test_path_prepend_decorator():
-    
-    class A:
-        path = None
-
-    def fun(path):
-        A.path = path
-        return path
-    
-    A.path = None
-    new_fun = filters.redirect_path('output')(fun)
-    output = new_fun('aa/bb')
-    assert A.path == 'output/aa/bb'
-    assert output == 'aa/bb'
-
-    A.path = None
-    new_fun = filters.redirect_path('ddd')(fun)
-    output = new_fun('cc/bb')
-    assert A.path == 'ddd/cc/bb'
-    assert output == 'cc/bb'
     
 
 def test_flatten_dict_works():
@@ -35,7 +16,7 @@ def test_flatten_dict_works():
             10: 'Bye',
         }
     }
-    observed = filters.flatten_nested_dict(data)
+    observed = data_structures.flatten_nested_dict(data)
     expected = {
         ('a', 1): True,
         ('a', 2): False,
@@ -55,7 +36,7 @@ def test_promote_key_list_of_dicts():
         {'A': {'a': 1, 'b': [10, 20, 30]}},
         {'A': {'a': 10, 'b': [100, 200, 300]}}
     ]
-    observed = filters.promote_key(data, key='newkey', attrs=['A', 'b', 1])
+    observed = data_structures.promote_key(data, key='newkey', attrs=['A', 'b', 1])
     expected = [
         {'A': {'a': 1, 'b': [10, 20, 30]}, 'newkey': 20},
         {'A': {'a': 10, 'b': [100, 200, 300]}, 'newkey': 200},
@@ -68,7 +49,7 @@ def test_promote_key_dict_of_dicts():
         'first': {'A': {'a': 1, 'b': [10, 20, 30]}},
         'second': {'A': {'a': 10, 'b': [100, 200, 300]}}
     }
-    observed = filters.promote_key(data, key='newkey', attrs=['A', 'b', 1])
+    observed = data_structures.promote_key(data, key='newkey', attrs=['A', 'b', 1])
     expected = {
         'first': {'A': {'a': 1, 'b': [10, 20, 30]}, 'newkey': 20},
         'second': {'A': {'a': 10, 'b': [100, 200, 300]}, 'newkey': 200},
@@ -78,7 +59,7 @@ def test_promote_key_dict_of_dicts():
 
 def test_multi_index():
     data = {'a': {'b': [10, 20, 30]}}
-    observed = filters.multi_index(data, ['a', 'b', 1])
+    observed = data_structures.multi_index(data, ['a', 'b', 1])
     expected = 20
     assert observed == expected
 
@@ -101,7 +82,7 @@ def test_sort_by():
 
     # When reversed, A should be descending
     # A should be ascending
-    observed = filters.sort_by(data, [1, 'a', 1, 'A'], reverse=True)
+    observed = data_structures.sort_by(data, [1, 'a', 1, 'A'], reverse=True)
     expected = [
         (12, {'a': [4, {'A': 200, 'B': 200}]}),
         (25, {'a': [4, {'A': 100, 'B': 200}]}),
@@ -109,11 +90,3 @@ def test_sort_by():
     ]
     assert observed == expected
     
-
-
-def test_prepend():
-    assert filters.prepend('a/b', 'z') == 'z/a/b'
-
-def test_prepend_does_not_work_on_absolute_paths():
-    with pytest.raises(ValueError):
-        filters.prepend('/a/b', 'z')
