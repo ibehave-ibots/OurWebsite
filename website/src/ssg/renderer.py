3d707ef1c9a0217c8891a44461832ddad5843f9d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-10 11:40:54 +0200

    md files with frontmatter work

--- website/src/ssg/renderer.py
+++ website/src/ssg/renderer.py
@@ -30,15 +30,35 @@ async def run_render_pipeline():
     site_data = await read_and_render_yaml_dir(base_dir='./site/data', env=env, data=global_data)
     
     # Walk through each 'pages' directory and render the pages found inside
-    async for page_dir in AsyncPath('./pages').glob('[!_]*'):
-        print(page_dir)
+    async for page_path in AsyncPath('./pages').glob('[!_]*/[!_]*.md'):
+        page_text = (await page_path.read_text()).strip()
 
-        env = build_jinja_environment(['./site/templates', page_dir])
+        print(f'Rendering: {page_path}')
+        if page_text.startswith('---'):
+            _, templated_yaml_text, md_text = page_text.split('---')
+            template = env.from_string(templated_yaml_text)
+            yaml_text = await template.render_async( data=global_data, site=site_data)
+            yaml_data = text_to_data(yaml_text, format='yaml')
+        else:
+            yaml_data = {}
+            md_text = page_text
+
+        md_html = text_to_data(md_text, 'md')                
+        page_data = {'data': yaml_data, 'content': md_html}
+
+
+
+        # Render HTML Template
+        env = build_jinja_environment(['./site/templates', page_path.parent])
         template = env.get_template('template.html')
-        url_path = page_dir.relative_to('./pages').with_suffix('.html')
+        url_path = page_path.relative_to('./pages').with_suffix('.html')
+        if url_path.name == 'index.html':
+            url_path = url_path.parent.with_suffix('.html')
+            
         page_html = await template.render_async(
             data=global_data, 
             site=site_data, 
+            page=page_data,
             url=str(PurePosixPath(url_path))
         )
 
