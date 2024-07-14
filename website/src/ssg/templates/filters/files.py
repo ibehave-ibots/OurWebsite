29ba0467adf99d401fcfb3ceb8ba28e8e108e8d6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-14 15:17:19 +0200

    new approach

--- website/src/ssg/templates/filters/files.py
+++ website/src/ssg/templates/filters/files.py
@@ -1,66 +1,81 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
+import hashlib
 import shutil
+from typing import Callable
+import typing
 import urllib.request
 from pathlib import Path, PurePosixPath
+if typing.TYPE_CHECKING:
+    from _hashlib import HASH
 
 from PIL import Image
 
 @dataclass(frozen=True)
 class ImageAssetManager:
-    template_dir: Path
-    src_basedir: Path = Path('./pages')
-    shared_static_dir: Path = Path('./shared/static')
-    build_basedir: Path = Path('./_output')
-    build_static_basedir: Path = Path('./_output/static')
-    _assetized: set[Path] = field(default_factory=set)
+    webserver_root: Path
+    output_path: Path
+    copyfun: Callable[[str, str], None] = shutil.copyfile
+    hashfun: Callable[[bytes], HASH] = hashlib.md5
 
 
     def asset(self, path: str) -> str:
-        orig_path = path
-        path = Path(path)
-        if 'http' in orig_path:
-            src_path = orig_path
-            target_path = self.build_basedir.joinpath('assets_external').joinpath(orig_path[orig_path.index('//') + 2:])
-            output_path = '/' + str(PurePosixPath(target_path.relative_to(self.build_basedir)))
-            method = 'download'
-        elif PurePosixPath(path).is_absolute():
-            src_path = self.shared_static_dir.joinpath(orig_path.lstrip('/'))
-            target_path = self.build_static_basedir.joinpath(orig_path.lstrip('/'))
-            output_path = orig_path
-            method = 'copy'
-            assert src_path.exists()
-        else:
-            src_path = Path(self.template_dir).joinpath(path)
-            target_path = self.build_basedir.joinpath(Path(self.template_dir).relative_to(self.src_basedir)).joinpath(path)
-            output_path = orig_path
-            method = 'copy'
-            assert src_path.exists()
+        path = PurePosixPath(path)
+        hash_str = self.hashfun(Path(path).read_bytes()).hexdigest()[:6]
+        fname_out = path.with_stem(path.stem + '_' + hash_str).name
+        save_path = self.output_path.joinpath(fname_out)
+        self.copyfun(str(PurePosixPath(path)), str(PurePosixPath(save_path)))
+        relative_asset_path = self.output_path.relative_to(self.webserver_root)
+        return str(PurePosixPath('/').joinpath(relative_asset_path).joinpath(fname_out))
+    
+
+        # orig_path = path
+        # path = Path(path)
+        # if 'http' in orig_path:
+        #     src_path = orig_path
+        #     target_path = self.build_basedir.joinpath('assets_external').joinpath(orig_path[orig_path.index('//') + 2:])
+        #     output_path = '/' + str(PurePosixPath(target_path.relative_to(self.build_basedir)))
+        #     method = 'download'
+        # elif PurePosixPath(path).is_absolute():
+        #     src_path = self.shared_static_dir.joinpath(orig_path.lstrip('/'))
+        #     target_path = self.build_static_basedir.joinpath(orig_path.lstrip('/'))
+        #     output_path = orig_path
+        #     method = 'copy'
+        #     assert src_path.exists()
+        # else:
+        #     src_path = Path(self.template_dir).joinpath(path)
+        #     target_path = self.build_basedir.joinpath(Path(self.template_dir).relative_to(self.src_basedir)).joinpath(path)
+        #     output_path = orig_path
+        #     method = 'copy'
+        #     assert src_path.exists()
             
-        if not target_path.exists():
-            target_path.parent.mkdir(parents=True, exist_ok=True)
-            if method == 'download':
-                urllib.request.urlretrieve(src_path, target_path)
-            elif method == 'copy':
-                shutil.copyfile(src_path, target_path)
-        self._assetized.add(output_path)
-        return output_path
+        # if not target_path.exists():
+        #     target_path.parent.mkdir(parents=True, exist_ok=True)
+        #     if method == 'download':
+        #         urllib.request.urlretrieve(src_path, target_path)
+        #     elif method == 'copy':
+        #         shutil.copyfile(src_path, target_path)
+        # self._assetized.add(output_path)
+        # return output_path
     
     def resize(self, path, width: int, height: int) -> str:
-        orig_path = path
-        path = Path(path)
-        if orig_path not in self._assetized:
-            raise ValueError(f"{path} must be assetized before resizing.  Call the `asset` filter first.")
-        if PurePosixPath(path).is_absolute():
-            src_path = self.build_basedir.joinpath(orig_path.lstrip('/'))
+        ...
+        # orig_path = path
+        # path = Path(path)
+        # if orig_path not in self._assetized:
+        #     raise ValueError(f"{path} must be assetized before resizing.  Call the `asset` filter first.")
+        # if PurePosixPath(path).is_absolute():
+        #     src_path = self.build_basedir.joinpath(orig_path.lstrip('/'))
             
-        else:
-            src_path = self.build_basedir.joinpath(Path(self.template_dir).relative_to(self.src_basedir)).joinpath(path)
+        # else:
+        #     src_path = self.build_basedir.joinpath(Path(self.template_dir).relative_to(self.src_basedir)).joinpath(path)
         
-        with Image.open(src_path) as img:
-            img2 = img.resize((width, height))
+        # with Image.open(src_path) as img:
+        #     img2 = img.resize((width, height))
 
-        new_stem = path.stem + f"_{width}x{height}"
-        target_path = src_path.with_stem(new_stem)
-        img2.save(target_path)
-        return str(PurePosixPath(orig_path).with_stem(new_stem))
+        # new_stem = path.stem + f"_{width}x{height}"
+        # target_path = src_path.with_stem(new_stem)
+        # img2.save(target_path)
+        # return str(PurePosixPath(orig_path).with_stem(new_stem))
         
--- website/src/ssg/templates/filters/files__test.py
+++ website/src/ssg/templates/filters/files__test.py
@@ -1,2 +1,39 @@
+from pathlib import Path, PurePosixPath
 import pytest
 from . import files
+from unittest.mock import Mock
+from hashlib import md5
+from _hashlib import HASH
+
+
+def test_asset_creates_hashed_asset_in_assets_dir(tmp_path_factory):
+    asset_path = tmp_path_factory.mktemp('assets')
+    webserver_root = tmp_path_factory.mktemp('output')
+    output_path = webserver_root / 'static'
+    
+    hashfun = Mock(HASH)
+    hashfun().hexdigest.return_value = 'ABCDEFGHIJKLMNOPQ'
+    
+
+    data_file = asset_path.joinpath('data.file')
+    data_file.touch()
+    
+    copyfun = Mock()
+    manager = files.ImageAssetManager(
+        webserver_root=webserver_root, 
+        output_path=output_path, 
+        copyfun=copyfun,
+        hashfun=hashfun,
+    )
+    
+    webserver_path = manager.asset(data_file)
+    assert webserver_path == '/static/data_ABCDEF.file'
+    copyfun.assert_called_once_with(
+        str(PurePosixPath(data_file)),
+        str(PurePosixPath(output_path).joinpath('data_ABCDEF.file')),
+    )
+
+
+
+
+    
