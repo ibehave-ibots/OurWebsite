5a3df678bf9618d9224605ac2e2df4097cd70210
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-15 10:49:58 +0200

    with fixture

--- website/src/ssg/templates/filters/files__test.py
+++ website/src/ssg/templates/filters/files__test.py
@@ -6,7 +6,8 @@ from hashlib import md5
 from _hashlib import HASH
 
 
-def test_asset_creates_hashed_asset_in_assets_dir(tmp_path_factory):
+@pytest.fixture
+def manager(tmp_path_factory) -> files.ImageAssetManager:
     asset_path = tmp_path_factory.mktemp('assets')
     webserver_root = tmp_path_factory.mktemp('output')
     output_path = webserver_root / 'static'
@@ -27,43 +28,33 @@ def test_asset_creates_hashed_asset_in_assets_dir(tmp_path_factory):
         downloadfun=downloadfun,
         hashfun=hashfun,
     )
-    
+    return manager
+
+
+
+def test_asset_creates_hashed_asset_in_assets_dir(tmp_path, manager):
+    data_file = tmp_path.joinpath('data.file')    
+    data_file.touch()
     webserver_path = manager.asset(data_file)
     assert webserver_path == '/static/data_ABCDEF.file'
-    copyfun.assert_called_once_with(
+    manager.copyfun.assert_called_once_with(
         str(PurePosixPath(data_file)),
-        str(PurePosixPath(output_path).joinpath('data_ABCDEF.file')),
+        str(PurePosixPath(manager.asset_path).joinpath('data_ABCDEF.file')),
     )
-    downloadfun.assert_not_called()
+    manager.downloadfun.assert_not_called()
 
 
-def test_downloaded_assets_create_hashed_asset_in_assets_dir_using_url(tmp_path_factory):
-    asset_path = tmp_path_factory.mktemp('assets')
-    webserver_root = tmp_path_factory.mktemp('output')
-    output_path = webserver_root / 'static'
-
-    hashfun = Mock(HASH)
-    hashfun().hexdigest.return_value = 'ABCDEFGHIJKLMNOPQ'
-
-    downloadfun = Mock()
-    copyfun = Mock()
+def test_downloaded_assets_create_hashed_asset_in_assets_dir_using_url(manager):
     
-    
-    manager = files.ImageAssetManager(
-        webserver_root=webserver_root, 
-        asset_path=output_path, 
-        downloadfun=downloadfun,
-        copyfun=copyfun,
-        hashfun=hashfun,
-    )
     url = 'http://website.com/dafadflkj/image.jpg'
     webserver_path = manager.asset(url)
     assert webserver_path == '/static/image_ABCDEF.jpg'
     
-    downloadfun.assert_called_once_with(
+    manager.downloadfun.assert_called_once_with(
         url,
-        str(PurePosixPath(output_path).joinpath('image_ABCDEF.jpg'))
+        str(PurePosixPath(manager.asset_path).joinpath('image_ABCDEF.jpg'))
     )
-    copyfun.assert_not_called()
+    
+    manager.copyfun.assert_not_called()
 
     
