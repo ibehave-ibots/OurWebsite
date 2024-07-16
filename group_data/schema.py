e720c60d5bf732716dc43bed6057d572e79874a6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-16 18:04:00 +0200

    validate orgs

--- group_data/schema.py
+++ group_data/schema.py
@@ -6,6 +6,8 @@ from yaml_dir_parser import load_dir
 
 class Data(BaseModel):
     people: dict[str, Person]
+    orgs: Orgs
+
 
 class Person(BaseModel):
     name: str
@@ -18,12 +20,27 @@ class Person(BaseModel):
     github_username: Optional[str] = None
     image: Optional[HttpUrl | FilePath] = None
 
-    
+
+class Orgs(BaseModel):
+    partners: list[Partner]
+
+class Partner(BaseModel):
+    name: str
+    url: HttpUrl
+    email: EmailStr
+    description: str
+    institute: Institute
+
+
+class Institute(BaseModel):
+    name: str
+    url: HttpUrl
 
 
 
 data = load_dir('data')
 dd = {}
 dd['people'] = data['people']
+dd['orgs'] = data['orgs']
 Data.model_validate(dd)
 # UserModel.model_validate(data)
