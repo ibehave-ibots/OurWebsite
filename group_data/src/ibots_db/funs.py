c8e66175db345ce0f0a0a26711bffd58c2201700
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-17 14:02:23 +0200

    update

--- group_data/src/ibots_db/funs.py
+++ group_data/src/ibots_db/funs.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 from pathlib import Path
+from typing import Any
 
 from yaml_dir_parser import load_dir
 
 from .schema import Data
-
+from pydantic import BaseModel
 
 DATA_PATH = Path(__file__).parent.parent.parent / 'data'
 
@@ -23,6 +24,23 @@ def check_for_validation_errors(path: Path = DATA_PATH, verbose=False) -> None:
     if verbose:
         print('...validated!')
     return None
+
+
+def update_all(key: str, data: dict[str, BaseModel]):
+    # check the key is an attribute of the Data schema. (Done)
+    # check that the BaseModel given is the same type specified by the schema. (Done)
+    
+    # make a temporary directory to try out the update (Done)
+
+    # try it in temp:
+        # copy the existing database into the temporary directory (Done)
+        # change all the data to yaml and write it in the correct place (key)
+        # check that that the temporary db reads properly and is valid
+    # try it in real:
+        # change all the data to yaml and write it in the correct place (key)
+        # check that that the real db reads properly and is valid
+
+    ...
 
 
 if __name__ == '__main__':
