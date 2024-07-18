ddeb09bb8a5dae83156f18612171d0487c35dabe
sangeethankumar <an.sangeetha@gmail.com>
2024-07-18 09:08:10 +0200

    delete old code

--- consulting_analysis/consult_repo/__init__.py
+++
@@ -1,2 +0,0 @@
-from src import DataDownload, ScieboDataDownload, TemplateDocumentProcessor, ScieboResultsUpload
-
--- consulting_analysis/consult_repo/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from src import ScieboDataDownload
-from src import TemplateDocumentProcessor
-
