fafaac59dc5f359e5d3bde5b49f07600ac164ce7
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-07-18 20:21:57 +0200

    brought back options for how to run the pipeline

--- website/main.py
+++ website/main.py
@@ -1,45 +1,33 @@
+import os
+from argparse import ArgumentParser
 
+import py7zr
 
-# Use ipdb as the default debugger, if possible.
-try:
-    import ipdb
-    import os
-    os.environ['PYTHONBREAKPOINT'] = 'ipdb.set_trace'
-except ImportError:
-    pass
+from ssg.api import build_dev_server, build_output
 
-
-import asyncio
-from dataclasses import dataclass
-from typing import Any, Coroutine
-from ssg.renderer import run_render_pipeline
-from ssg.server import build_server, TornadoEventLoopCallable
-import shutil
 
 if not os.path.exists('theme'):
     if not os.path.exists('remote_files/theme.7z'):
         raise FileNotFoundError("Need theme.7z.  Be sure to run dvc pull to get the file.")
-    import py7zr
     print('extracting theme.7z to theme...')
     with py7zr.SevenZipFile('remote_files/theme.7z') as f:
         f.extract('.', ['theme'], recursive=True)
 
 
-if not os.path.exists('../group_data/data'):
+if not os.path.exists('./data'):
     raise FileNotFoundError("Need group database.  Be sure to run dvc pull to get it.")
 
 
+parser = ArgumentParser()
+parser.add_argument('command', choices=['serve', 'render'])
+args = parser.parse_args()
 
-page_builders = asyncio.run(run_render_pipeline())
-
-
-# breakpoint()
-
-
-server = build_server()
-for md_path, (coro, args) in page_builders.items():
-    server.watch(md_path, TornadoEventLoopCallable(coro, args), delay=3)
-    
-server.serve()
+if args.command == 'serve':
+    server = build_dev_server()
+    server.serve()
+elif args.command == 'render':
+    build_output()
+else:
+    raise ValueError("not a valid option.")
 
 
