d1558ee33882543ca269c904ec5882fd404af6db
sangeethankumar <an.sangeetha@gmail.com>
2024-07-19 14:37:38 +0200

    move to another folder

--- consulting_reports_extraction/dodo.py
+++ consulting_reports_extraction/dodo.py
@@ -5,7 +5,7 @@ def task_download():
     for consultant_name in consultant_names:
         yield {
             'name': f'{consultant_name}_download',
-            'actions': [f'papermill -p consultant_name {consultant_name} "Download.ipynb" "papermill_reports/download/{consultant_name}.ipynb"']
+            'actions': [f'papermill -p consultant_name {consultant_name} "notebooks/Download.ipynb" "papermill_reports/download/{consultant_name}.ipynb"']
         }
 
 def task_extract():
@@ -13,6 +13,6 @@ def task_extract():
     for consultant_name in consultant_names:
         yield {
             'name': f'{consultant_name}_extract',
-            'actions': [f'papermill -p consultant_name {consultant_name} "Extract.ipynb" "papermill_reports/extract/{consultant_name}.ipynb"'],
+            'actions': [f'papermill -p consultant_name {consultant_name} "notebooks/Extract.ipynb" "papermill_reports/extract/{consultant_name}.ipynb"'],
             'task_dep': ['download']
         }
