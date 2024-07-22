8de01adb98fa43aba820e5d33a3ee03f199ca65e
sangeethankumar <an.sangeetha@gmail.com>
2024-07-22 16:53:10 +0200

    doit back to running

--- consulting_reports_extraction/dodo.py
+++ consulting_reports_extraction/dodo.py
@@ -6,37 +6,28 @@ def task_download():
     """Downloading consulting reports"""
 
     for consultant_name in consultant_names:
-        download_files = [f'data/raw/{consultant_name}.docx']
-
-        def check_uptodate():
-            return all(Path(file).exists for file in download_files)
-
         yield {
             'name': f'{consultant_name}_download',
             'actions': [f'papermill -p consultant_name {consultant_name} "Download.ipynb" "papermill_reports/download/{consultant_name}.ipynb"'],
-            'targets': download_files,
-            'uptodate': [check_uptodate]
         }
 
 def task_extract():
     """Extracting entries"""
     for consultant_name in consultant_names:
-        extract_files = [file for file in Path('data/entries/{consultant_name}/').glob('*.txt')]
-
-        def check_uptodate():
-            return all(Path(file).exists for file in extract_files)
-
         yield {
             'name': f'{consultant_name}_extract',
             'actions': [f'papermill -p consultant_name {consultant_name} "Extract.ipynb" "papermill_reports/extract/{consultant_name}.ipynb"'],
-            'targets': extract_files,
-            'uptodate': [check_uptodate]
         }
 
 def task_upload():
     """Upload entries"""
+
     for consultant_name in consultant_names:
-        yield {
-            'name': f'{consultant_name}_upload',
-            'actions': [f'papermill -p consultant_name {consultant_name} "Upload.ipynb" "papermill_reports/upload/{consultant_name}.ipynb"'],
-        }
+        Path(f'papermill_reports/upload/{consultant_name}').mkdir(exist_ok=True)
+        extract_files = [file for file in Path(f'data/entries/{consultant_name}/').glob('*.txt')]
+        for file in extract_files:
+            yield {
+                'name': f'{consultant_name}_{file.stem}_upload',
+                'actions': [f'papermill -p consultant_name {consultant_name} -p entry_num {file.stem} "Upload.ipynb" "papermill_reports/upload/{consultant_name}/{file.stem}.ipynb"'],
+
+            }
