2a85edbbaccf83d6918a704e0401fb7744496d96
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-08-06 22:14:54 +0200

    removed group data

--- group_data/scripts/validate_db.py
+++
@@ -1,3 +0,0 @@
-import ibots_db
-
-ibots_db.check_for_validation_errors(verbose=True)
--- group_data/src/ibots_db/__init__.py
+++
@@ -1,3 +0,0 @@
-from .schema import Data
-from .funs import load, check_for_validation_errors, copy_to
-from .updates import update_all
